fe631decb168f904c018291831a58bbe28257669
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-03-09 00:19:41 -0500

    Release 0: Prepare Files

---
+++ week-5/5.5-Separate_Numbers_with_Commas/my_solution.rb
@@ -0,0 +1,36 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and include it in this file. Also make sure everything that isn't code is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input?
+# Positive integer
+# What is the output? (i.e. What should the code return?)
+# Comma-separated integer as a STRING
+# What are the steps needed to solve the problem?
+
+# separate_comma(1000)     #=> "1,000"
+# separate_comma(1000000) # => "1,000,000"
+# separate_comma(0)       # => "0"
+# separate_comma(100)     # => "100"
+
+Anything < 1,000 can return input as string, no commas
+Anything < 1,000,000 will have one comma
+
+
+# 1. Initial Solution
+def separate_comma(integer)
+
+end
+
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
